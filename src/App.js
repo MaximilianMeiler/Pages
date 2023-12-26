2e61f00b6619b9a05e1527a525286726f7f44ee5
MaximilianMeiler <maximilianmeiler@ufl.edu>
2023-12-25 22:57:59 -0500

    idk

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+
 
 function App() {
+  const [stack, setStack] = useState([-1])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='trueBackground'></div>
+      <div className='background'>
+        <div className='left'>
+          {stack[stack.length-1] === 0 ? 
+            <div>This is the about page</div>
+
+            : stack[stack.length-1] === 0 ? <></> : <></>
+          }
+
+        </div>
+        <div className='top'></div>
+        <div className='right'>
+          <div onClick={() => setStack([-1, 0])}>About</div>
+          <div onClick={() => setStack([-1, 1])}>Activity</div>
+          <div onClick={() => setStack([-1, 2])}>Projects</div>
+          <div onClick={() => setStack([-1, 3])}>Skills</div>
+          <div onClick={() => setStack([-1, 4])}>Contact</div>
+        </div>
+      </div>
     </div>
   );
 }

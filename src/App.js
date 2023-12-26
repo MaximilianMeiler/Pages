96d8a700b3fbe3e30fc8ed5189462ad0cb099a70
MaximilianMeiler <maximilianmeiler@ufl.edu>
2023-12-26 10:07:55 -0500

    Colors

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import './App.css';
 import {useEffect, useState} from 'react';
 
 function App() {
-  const [stack, setStack] = useState([-1, 0, 1, 2])
+  const [stack, setStack] = useState([0])
   const colors = [150, 20, 190, 320, 45]
 
-  async function popStack() {
+  async function popStack(delay) {
     const curr = document.querySelector(".left");
-    const prev = document.querySelector(".leftBehind");
     curr.classList.add("hidden");
 
     setTimeout(() => {
       setStack(stack.slice(0, stack.length-1))
       curr.classList.remove("hidden");
-    }, 250)
+    }, delay)
   }
 
   useEffect(() => {
@@ -24,35 +23,34 @@ function App() {
     <div>
       <div className='trueBackground'></div>
       <div className='background'>
-        <div className='left'>
-          {stack[stack.length-1] === -1 ? 
-            <div>This is the home page</div>
-          : stack[stack.length-1] === 0 ? 
-            <div>This is the about page</div> 
+        <div className='left' style={stack.length > 1 ? {backgroundColor:`hsl(${colors[stack[stack.length-1]]}, 100%, 50%)`} : {}}>
+          {stack[stack.length-1] === 0 ? 
+            <div>This is the about page</div>
+          : stack[stack.length-1] === 1 ? 
+            <div>This is the activity page</div> 
           : <div>Error 404</div>
           }
 
         </div>
 
         {stack.length > 1 ? 
-        <div className='leftBehind'>
-          {stack[stack.length-2] === -1 ? 
-            <div>This is the home page</div>
-          : stack[stack.length-2] === 0 ? 
-            <div>This is the about page</div> 
+        <div className='leftBehind' style={stack.length > 2 ? {backgroundColor:`hsl(${colors[stack[stack.length-2]]}, 100%, 50%)`} : {}}>
+          {stack[stack.length-2] === 0 ? 
+            <div>This is the about page</div>
+          : stack[stack.length-2] === 1 ? 
+            <div>This is the activity page</div> 
           : <div>Error 404</div>
           }
         </div>
         : <></>}
 
-        <div className='top'></div>
-        <div className='right'>
-          <div onClick={() => setStack([-1, 0])}>About</div>
-          <div onClick={() => setStack([-1, 1])}>Activity</div>
-          <div onClick={() => setStack([-1, 2])}>Projects</div>
-          <div onClick={() => setStack([-1, 3])}>Skills</div>
-          <div onClick={() => setStack([-1, 4])}>Contact</div>
-          <button onClick={() => popStack()}>pop!</button>
+        <div className='top' style={stack.length > 1 ? {backgroundColor:`hsl(${colors[stack[stack.length-1]]}, 100%, 45%)`} : {}}></div>
+        <div className='right' style={stack.length > 1 ? {backgroundColor:`hsl(${colors[stack[stack.length-1]]}, 100%, 40%)`} : {}}>
+          <div onClick={() => setStack([0, 1])}>Activity</div>
+          <div onClick={() => setStack([0, 2])}>Projects</div>
+          <div onClick={() => setStack([0, 3])}>Skills</div>
+          <div onClick={() => setStack([0, 4])}>Contact</div>
+          <button onClick={() => popStack(250)}>pop!</button>
         </div>
       </div>
     </div>

19bb1f4b9dc50a9e420e304f7f189982e1ef3a9d
MaximilianMeiler <maximilianmeiler@ufl.edu>
2024-01-04 12:03:05 -0500

    Skills page complete

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,39 +11,39 @@ export default function Skills({
 
         <div className="projectRow">
           <div className="genericContainer">
-            <img className='image' src={'./Images/cat-spaghetti.jpg'} alt='bruh'></img>
-            <div className='projTitle'>gpTA</div>
+            <img className='black' src={'./Images/html.png'} alt='bruh'></img>
+            <div className='projTitle'>HTML/CSS</div>
           </div>
           <div className="genericContainer">
-            <img className='image' src={'./Images/cat-spaghetti.jpg'} alt='bruh'></img>
-            <div className='projTitle'>gpTA</div>
+            <img className='black' src={'./Images/js.png'} alt='bruh'></img>
+            <div className='projTitle'>Javascript</div>
           </div>
           <div className="genericContainer">
-            <img className='image' src={'./Images/cat-spaghetti.jpg'} alt='bruh'></img>
-            <div className='projTitle'>gpTA</div>
+            <img className='black' src={'./Images/python.png'} alt='bruh'></img>
+            <div className='projTitle'>Python</div>
           </div>
           <div className="genericContainer">
-            <img className='image' src={'./Images/cat-spaghetti.jpg'} alt='bruh'></img>
-            <div className='projTitle'>gpTA</div>
+            <img className='black' src={'./Images/cpp.png'} alt='bruh'></img>
+            <div className='projTitle'>C++</div>
           </div>
         </div>
 
         <div className="projectRow">
           <div className="genericContainer">
-            <img className='image' src={'./Images/cat-spaghetti.jpg'} alt='bruh'></img>
-            <div className='projTitle'>gpTA</div>
+            <img className='black' src={'./Images/react.png'} alt='bruh'></img>
+            <div className='projTitle'>React</div>
           </div>
           <div className="genericContainer">
-            <img className='image' src={'./Images/cat-spaghetti.jpg'} alt='bruh'></img>
-            <div className='projTitle'>gpTA</div>
+            <img className='black' src={'./Images/nodejs.png'} alt='bruh'></img>
+            <div className='projTitle'>Node.js</div>
           </div>
           <div className="genericContainer">
-            <img className='image' src={'./Images/cat-spaghetti.jpg'} alt='bruh'></img>
-            <div className='projTitle'>gpTA</div>
+            <img className='black' src={'./Images/mongo.png'} alt='bruh'></img>
+            <div className='projTitle'>MongoDB</div>
           </div>              
           <div className="genericContainer">
-            <img className='image' src={'./Images/cat-spaghetti.jpg'} alt='bruh'></img>
-            <div className='projTitle'>gpTA</div>
+            <img className='black' src={'./Images/github.png'} alt='bruh'></img>
+            <div className='projTitle'>GitHub</div>
           </div>
         </div>
       </div>

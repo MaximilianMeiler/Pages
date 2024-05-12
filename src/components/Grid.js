c39732b5954ca3e1af057ecfa3521b4a84d31e5f
MaximilianMeiler <maximilianmeiler@ufl.edu>
2024-05-11 23:02:32 -0400

    Grid separation

---
+++ src/components/Grid.js
@@ -0,0 +1,43 @@
+import './GridScreen.css';
+
+export default function Grid({
+  pushStack, color, value, index
+}) {
+  const bgStyle = {
+    "--color": color
+  }
+
+  return (
+    <div className='grid'>
+
+      {value.data.map((d, i) => {
+        return (
+        <div className={d.page ? 'container clickable' : 'container'} onMouseEnter={() => {
+          var top = document.querySelector(`#t${index}-${i}`);
+          var right = document.querySelector(`#r${index}-${i}`);
+          var proj = document.querySelector(`#p${index}-${i}`);
+
+          top.classList.add("showTop");
+          right.classList.add("showRight");
+          proj.classList.add("showProj");
+        }} onMouseLeave={() => {
+          var top = document.querySelector(`#t${index}-${i}`);
+          var right = document.querySelector(`#r${index}-${i}`);
+          var proj = document.querySelector(`#p${index}-${i}`);
+
+          top.classList.remove("showTop");
+          right.classList.remove("showRight");
+          proj.classList.remove("showProj");
+        }} onClick={d.page ? () => {pushStack(d.page)} : () => {}}>
+            <div className="project" id={`p${index}-${i}`} style={bgStyle}>
+              <img className='image' src={`./Images/${d.image}.png`} alt='Project logo'></img>
+              <div className='projTitle'>{d.title}</div>
+            </div>
+            <div className="pTop" id={`t${index}-${i}`} style={{backgroundColor: `hsl(${color}, 100%, 80%)`}}></div>
+            <div className="pRight" id={`r${index}-${i}`} style={{backgroundColor: `hsl(${color}, 100%, 75%)`}}></div>
+        </div>  
+      )})}
+
+    </div>
+  )
+}
--- src/components/GridScreen.js
+++ src/components/GridScreen.js
@@ -1,4 +1,5 @@
 import './GridScreen.css';
+import Grid from './Grid';
 
 export default function GridScreen({
   pushStack, color, values
@@ -13,41 +14,10 @@ export default function GridScreen({
         return (
           <div className="group" style={{flex: v.flex}}>
             <div className='header'>{v.title}</div>
-            <div className='grid'>
-
-              {v.data.map((d, i) => {
-                return (
-                <div className={d.page ? 'container clickable' : 'container'} onMouseEnter={() => {
-                  var top = document.querySelector(`#t${j}-${i}`);
-                  var right = document.querySelector(`#r${j}-${i}`);
-                  var proj = document.querySelector(`#p${j}-${i}`);
-
-                  top.classList.add("showTop");
-                  right.classList.add("showRight");
-                  proj.classList.add("showProj");
-                }} onMouseLeave={() => {
-                  var top = document.querySelector(`#t${j}-${i}`);
-                  var right = document.querySelector(`#r${j}-${i}`);
-                  var proj = document.querySelector(`#p${j}-${i}`);
-
-                  top.classList.remove("showTop");
-                  right.classList.remove("showRight");
-                  proj.classList.remove("showProj");
-                }} onClick={d.page ? () => {pushStack(d.page)} : () => {}}>
-                    <div className="project" id={`p${j}-${i}`} style={bgStyle}>
-                      <img className='image' src={`./Images/${d.image}.png`} alt='Project logo'></img>
-                      <div className='projTitle'>{d.title}</div>
-                    </div>
-                    <div className="pTop" id={`t${j}-${i}`} style={{backgroundColor: `hsl(${color}, 100%, 80%)`}}></div>
-                    <div className="pRight" id={`r${j}-${i}`} style={{backgroundColor: `hsl(${color}, 100%, 75%)`}}></div>
-                </div>  
-              )})}
-
-            </div>
+            <Grid pushStack={pushStack} color={color} value={v} index={j}/>
           </div>
         )
       })}
-
 
     </div>
   )
